c43bfec60515d9f583230e30ef6edcf0a916f4cf
Oskar-Mikael <oskar.m.bostrom@gmail.com>
2021-08-11 16:00:39 +0200

    Login and registration forms, and remade validation

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -3,6 +3,20 @@
      <h2>
        Login
      </h2>
+     <form method="post" @submit.prevent="login">
+            <p>
+                Email
+            </p>
+          <input type="email" name="email" v-model="form.email"/>
+            <p>
+                Password
+            </p>
+          <input type="password" name="password" v-model="form.password"/>
+          <button type="submit">Login</button>
+      </form>
+        <p class="errors" v-if="errors">
+          {{ errors }}
+        </p>
      <h4>
        <router-link to="/register">
        Not registered?
@@ -12,11 +26,40 @@
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-  name: 'Login'
+  name: 'Login',
+
+  data () {
+    return {
+      form: {
+        email: '',
+        password: ''
+      },
+      errors: ''
+    }
+  },
+
+  methods: {
+    login () {
+      axios
+        .post('http://localhost:5000/api/users/login', this.form)
+        .then((res) => {
+          console.log(res)
+          localStorage.setItem('token', res.data.token)
+        })
+        .catch((err) => {
+          this.errors = err.response.data.message
+          console.log(err.response)
+        })
+    }
+  }
 }
 </script>
 
 <style>
+  .errors {
+    color: red;
+  }
 
 </style>
