52e7b355f76e7d61a305d274605ad6fe98a1574a
Oskar-Mikael <oskar.m.bostrom@gmail.com>
2021-08-28 15:40:42 +0200

    Get posts of selected user, and display them on the profile page

--- frontend/src/components/Profile.vue
+++ frontend/src/components/Profile.vue
@@ -3,15 +3,51 @@
       <h2>
           {{ this.SelectedUser.message.username }}
       </h2>
+      <p>
+        Posts
+      </p>
+      <p>
+        Post count: {{ userPosts.length }}
+      </p>
+      <p @click="postNavigate(post._id)" v-for="post in userPosts" :key="post._id">
+        {{ post.title }}
+      </p>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapGetters, mapActions } from 'vuex'
+import axios from 'axios'
 export default {
   name: 'Profile',
 
-  computed: mapGetters(['SelectedUser'])
+  computed: mapGetters(['SelectedUser']),
+
+  data () {
+    return {
+      userPosts: ''
+    }
+  },
+
+  mounted () {
+    this.getUser()
+  },
+
+  methods: {
+    ...mapActions(['navigateToPost']),
+
+    getUser () {
+      axios.get('posts/user/' + this.SelectedUser.message._id)
+        .then(res => {
+          this.userPosts = res.data.message
+          console.log(res)
+        })
+    },
+
+    postNavigate (id) {
+      this.navigateToPost(id)
+    }
+  }
 }
 </script>
 
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { getPosts, createPost, getPost, updatePost, deletePost, addComment, deleteComment, likePost, unlikePost, getPostsByCategory, searchPosts } from '../../controllers/posts.js';
+import { getPosts, createPost, getPost, updatePost, deletePost, addComment, deleteComment, likePost, unlikePost, getPostsByCategory, searchPosts, getUsersPosts } from '../../controllers/posts.js';
 import requireAuth from '../../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.get('/', getPosts);
+router.get('/user/:id', getUsersPosts);
 router.get('/category', getPostsByCategory);
 router.get('/search', searchPosts);
 router.get('/:id', getPost);
