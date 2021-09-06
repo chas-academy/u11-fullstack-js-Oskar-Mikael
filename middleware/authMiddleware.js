f4d3fc5a4930ee89474384789584f1cb597b8bab
Oskar-Mikael <oskar.m.bostrom@gmail.com>
2021-09-06 18:01:09 +0200

    Use environment variable

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -4,7 +4,7 @@ const requireAuth = (req, res, next) => {
     const token = req.headers.authorization
 
     if (token) {
-        jwt.verify(token, 'mv239@0mv8!230*9!90rttbn23w2g534', (err, decodedToken) => {
+        jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
             if (err) {
                 res.status(404).json({ error: 'You are not authorized' })
                 console.log(err)
