322585995bf789de53d1310e90a5c3ca2194bdfe
Oskar-Mikael <oskar.m.bostrom@gmail.com>
2021-09-15 13:39:15 +0200

    Trim input

--- models/post.js
+++ models/post.js
@@ -4,11 +4,13 @@ import Comment from './comment.js';
 const postSchema = mongoose.Schema({
   title: {
     type: String,
-    required: [true, 'Title field is required']
+    required: [true, 'Title field is required'],
+    trim: true,
   },
   body: {
     type: String,
-    required: [true, 'Body field is required']
+    required: [true, 'Body field is required'],
+    trim: true,
   },
   creator: {},
   likeCount: {
