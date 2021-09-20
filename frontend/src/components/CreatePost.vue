2a8eb1dd857eb38d0fc7df81dce4d00d1a4da9f5
Oskar-Mikael <oskar.m.bostrom@gmail.com>
2021-09-20 16:04:54 +0200

    Styling for error messages

--- frontend/src/components/CreatePost.vue
+++ frontend/src/components/CreatePost.vue
@@ -4,7 +4,7 @@
           Create Post
       </h2>
       <form @submit.prevent="addPost">
-        <select class="mb-8 text-black bg-gray-200 pl-2 md:w-1/4 w-8/12 h-10 rounded-md"  v-model="form.category">
+        <select class="text-black bg-gray-200 pl-2 md:w-1/4 w-8/12 h-10 rounded-md"  v-model="form.category">
           <option selected value=''>
             -Select category-
           </option>
@@ -15,12 +15,12 @@
         <p v-if="this.postErrors && this.postErrors.type === 'category'">
             {{ this.postErrors.message }}
           </p>
-          <input placeholder="Title" class="mb-8 text-black bg-gray-200 pl-2 md:w-1/4 w-8/12 h-10 rounded-md"  type="text" v-model="form.title"><br>
-          <p v-if="this.postErrors && this.postErrors.type === 'title'">
+          <input placeholder="Title" class="mt-8 text-black bg-gray-200 pl-2 md:w-1/4 w-8/12 h-10 rounded-md"  type="text" v-model="form.title"><br>
+          <p class="text-red-600 mt-2" v-if="this.postErrors && this.postErrors.type === 'title'">
             {{ this.postErrors.message }}
           </p>
-          <textarea placeholder="Body" class="mb-8 text-black bg-gray-200 pl-2 w-1/2 h-72 rounded-md"  type="text" v-model="form.body"/><br>
-          <p v-if="this.postErrors && this.postErrors.type === 'body'">
+          <textarea placeholder="Body" class="mt-8 text-black bg-gray-200 pl-2 w-1/2 h-72 rounded-md"  type="text" v-model="form.body"/><br>
+          <p class="text-red-600 mt-2" v-if="this.postErrors && this.postErrors.type === 'body'">
             {{ this.postErrors.message }}
           </p>
           <button class="my-6 py-2 px-20 bg-green-600 rounded-md" type="submit">Create Post</button>
